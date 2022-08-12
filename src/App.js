d73efb634cf909009a168b6d2727343e7486b8dd
yugmani <yugmani@yahoo.com>
2022-08-11 23:53:39 -0700

    parent component created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 import ChildCompA from './components/ChildCompA';
 import ChildCompB from './components/ChildCompB';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Lifting State up in ReactJS!</h1>
-      <ChildCompA />
-      <ChildCompB />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+    };
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+  }
+
+  handleTextChange(newText) {
+    this.setState({
+      text: newText,
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Lifting State up in ReactJS!</h1>
+        <ChildCompA
+          text={this.state.text}
+          handleTextChange={this.handleTextChange}
+        />
+        <ChildCompB />
+      </React.Fragment>
+    );
+  }
 }
+
+export default App;
